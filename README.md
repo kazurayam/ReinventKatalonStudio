da5a1cc750a0f03cbc9a4202bf53e4702b5526f0
kazurayam <kazuaki.urayama@gmail.com>
2019-12-14 10:39:56 +0900

    update

---
+++ README.md
@@ -0,0 +1,12 @@
+A practice in Katalon Studio: callExternalScript()
+========
+
+This is a Katalon Studio project for demonstration purpose.
+
+This project preposes a solution to a discussion raised at the following Forum discussion:
+[Executing a method from an array](https://forum.katalon.com/t/executing-a-method-from-an-array/37473/)
+
+
+`Test Cases/GroovyShell/main` is capable of executiung Groovy scripts located outside the Katalon project.
+
+Please have a look at [this script](Scripts/GroovyShell/main/Script1576283878899.groovy).
--- Scripts/GroovyShell/main/Script1576283878899.groovy
+++ Scripts/GroovyShell/main/Script1576283878899.groovy
@@ -5,18 +5,23 @@ import java.nio.file.Paths
 import com.kms.katalon.core.configuration.RunConfiguration
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
-Path findExternalScript(String scriptName) {
+Path findExternalScript(Script scriptName) {
 	Path project = Paths.get(RunConfiguration.getProjectDir())
-	Path scripts = project.resolve('Include/externalScripts')
+	return findExternalScript(project, scriptName)
+}
+Path findExternalScript(Path basedir, String scriptName) {
+	Path scripts = basedir.resolve('Include/externalScripts')
 	Path script = scripts.resolve(scriptName)
 	if (!Files.exists(script))
 		throw new FileNotFoundException(script.toString())
 	return script
 }
-
 List<Path> findExternalScripts() {
 	Path project = Paths.get(RunConfiguration.getProjectDir())
-	Path scripts = project.resolve('Include/externalScripts')
+}
+List<Path> findExternalScripts(Path basedir) {
+	Path project = Paths.get(RunConfiguration.getProjectDir())
+	Path scripts = basedir.resolve('Include/externalScripts')
 	return Files.walk(scripts)
 		.filter { item -> Files.isRegularFile(item) && item.toString().endsWith('.groovy') }
 		.collect();
@@ -37,3 +42,7 @@ WebUI.comment(">>> activating all scrits")
 for (Path f in findExternalScripts()) {
 	callExternalScript(f)
 }
+
+// Execute scripts outside the project
+//Path dir = Paths.get("whatever/path/you/want")
+//callExternalScript(findExternalScript(dir))
